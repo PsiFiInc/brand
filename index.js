60bde431ab006a819cbbd07fb20934efb8ca08ec
jtk.eth <jesperkristensen58@users.noreply.github.com>
2024-03-29 13:03:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ document.addEventListener("adobe_dc_view_sdk.ready", function () {
     /* Initialize the AdobeDC View object */
     var adobeDCView = new AdobeDC.View({
         /* Pass your registered client id */
-        clientId: "87f1da9b0e2e4e59aa2b8e2ec78674fe",
+        clientId: "d91e020d09fa4397bce4d246054736ae",
         /* Pass the div id in which PDF should be rendered */
         divId: "adobe-dc-view",
     });
