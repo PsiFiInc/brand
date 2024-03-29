87ef2dd2ccbcb113777c2f146c3337b470c93c59
jtk.eth <jesperkristensen58@users.noreply.github.com>
2024-03-29 14:38:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ document.addEventListener("adobe_dc_view_sdk.ready", function () {
         content: {
             /* Location of file where it is hosted */
             location: {
-                url: "https://github.com/PsiFiInc/brand/raw/main/PsiFi%20Brand%20Guide.pdf?download=",
+                url: "https://github.com/PsiFiInc/brand/raw/main/PsiFi Brand Guide.pdf?download=",
                 /*
                 If the file URL requires some additional headers, then it can be passed as follows:-
                 headers: [
